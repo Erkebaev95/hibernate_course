f87e1205e1029b7b44528155184ab3fc3eeb3b51
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2023-02-10 18:32:33 +0600

    used SequenceGenerator for practice.

--- src/main/java/org/erkebaev/App.java
+++ src/main/java/org/erkebaev/App.java
@@ -18,9 +18,13 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+            Person person1 = new Person("Ivan", 31);
+            Person person2 = new Person("Arman", 24);
+            Person person3 = new Person("Lena", 43);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
 
             session.getTransaction().commit();
 
